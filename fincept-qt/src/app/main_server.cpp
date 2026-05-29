d5c8c1be0c44a663aabbad1a12e6c2ae2221a09a
Leonard Langsdorf <leonard.langsdorf@gmail.com>
2026-05-29 16:06:19 -0400

    fix: address follow-up PR review feedback

--- fincept-qt/src/app/main_server.cpp
+++ fincept-qt/src/app/main_server.cpp
@@ -13,6 +13,13 @@ int main(int argc, char* argv[]) {
     fincept::PhaseOneServerCliOptions options = cli.options;
     options.mode = fincept::PhaseOneProcessMode::Server;
 
+    if (!options.client_passthrough_args.isEmpty()) {
+        fprintf(stderr,
+                "[PhaseOneServerCli] unexpected client arguments in server mode: %s\n",
+                qUtf8Printable(options.client_passthrough_args.join(' ')));
+        return 1;
+    }
+
     fincept::PhaseOneStartupCoordinator startup_coordinator;
     startup_coordinator.initialize_pre_app();
     if (const auto server_exit = startup_coordinator.maybe_run(argc, argv, options); server_exit.has_value())
